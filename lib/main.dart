0485595b16df42db1732d294cec3681acb0cbf36
= <sellauno03@gmail.com>
2021-02-19 16:04:15 +0700

    New Class Berita

--- lib/main.dart
+++ lib/main.dart
@@ -44,37 +44,7 @@ class MyApp extends StatelessWidget{
             height: 50.0,
             padding: const EdgeInsets.all(10.0),  
           ),
-          Row(
-            children: [
-              Container(
-                alignment:Alignment.center,
-                child: Image(image: NetworkImage('https://images.daznservices.com/di/library/GOAL/c0/68/gerard-pique-barcelona-yellow-card-2020-21_1b5pixs2oir8s1hy0nwfy1tnrz.jpg?t=1931977831&amp;quality=60&amp;w=700'),
-                fit: BoxFit.fitWidth,
-                ),
-                height: 80.0, width: 150.0,
-                decoration: BoxDecoration(
-                  border: Border.all (
-                  color: Colors.blueGrey,
-                  width: 1,
-                ),
-                ),
-                margin: const EdgeInsets.only(top: 10),
-              ),
-              Container(
-                alignment:Alignment.center,
-                child: Text("Pique Bilang Wasit Untungkan Madrid, Koeman Tepok Jidat", style: TextStyle(fontSize:15,color: Colors.black)),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                  color: Colors.blueGrey,
-                  width: 1,
-                ),
-                ),
-                height: 80.0, width: 210.0,
-                margin: const EdgeInsets.only(top: 10),
-                padding: const EdgeInsets.all(5.0),      
-              ),
-            ]
-          ),
+          Berita(),
           Container(
             alignment:Alignment.centerLeft,
             child: Text("Barcelona Feb 13,2021", style: TextStyle(fontSize:15,color:
@@ -89,39 +59,7 @@ class MyApp extends StatelessWidget{
             margin: const EdgeInsets.only(bottom:10,), 
             padding: const EdgeInsets.all(10.0),      
           ),
-
-          //222222222222222222222
-          Row(
-            children: [
-              Container(
-                alignment:Alignment.center,
-                child: Image(image: NetworkImage('https://images.daznservices.com/di/library/GOAL/c0/68/gerard-pique-barcelona-yellow-card-2020-21_1b5pixs2oir8s1hy0nwfy1tnrz.jpg?t=1931977831&amp;quality=60&amp;w=700'),
-                fit: BoxFit.fitWidth,
-                ),
-                height: 80.0, width: 150.0,
-                decoration: BoxDecoration(
-                  border: Border.all (
-                  color: Colors.blueGrey,
-                  width: 1,
-                ),
-                ),
-                margin: const EdgeInsets.only(top: 10),
-              ),
-              Container(
-                alignment:Alignment.center,
-                child: Text("Pique Bilang Wasit Untungkan Madrid, Koeman Tepok Jidat", style: TextStyle(fontSize:15,color: Colors.black)),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                  color: Colors.blueGrey,
-                  width: 1,
-                ),
-                ),
-                height: 80.0, width: 210.0,
-                margin: const EdgeInsets.only(top: 10),
-                padding: const EdgeInsets.all(5.0),      
-              ),
-            ]
-          ),
+          Berita(),          
           Container(
             alignment:Alignment.centerLeft,
             child: Text("Barcelona Feb 13,2021", style: TextStyle(fontSize:15,color:
@@ -136,9 +74,46 @@ class MyApp extends StatelessWidget{
             margin: const EdgeInsets.only(bottom:10,), 
             padding: const EdgeInsets.all(10.0),      
           ),
+          
         ]
         ),
       )
     );
-  }
+  }  
+}
+
+class Berita extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Row(
+            children: [
+              Container(
+                alignment:Alignment.center,
+                child: Image(image: NetworkImage('https://images.daznservices.com/di/library/GOAL/c0/68/gerard-pique-barcelona-yellow-card-2020-21_1b5pixs2oir8s1hy0nwfy1tnrz.jpg?t=1931977831&amp;quality=60&amp;w=700'),
+                fit: BoxFit.fitWidth,
+                ),
+                height: 80.0, width: 150.0,
+                decoration: BoxDecoration(
+                  border: Border.all (
+                  color: Colors.blueGrey,
+                  width: 1,
+                ),
+                ),
+                margin: const EdgeInsets.only(top: 10),
+              ),
+              Container(
+                alignment:Alignment.center,
+                child: Text("Pique Bilang Wasit Untungkan Madrid, Koeman Tepok Jidat", style: TextStyle(fontSize:15,color: Colors.black)),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                  color: Colors.blueGrey,
+                  width: 1,
+                ),
+                ),
+                height: 80.0, width: 210.0,
+                margin: const EdgeInsets.only(top: 10),
+                padding: const EdgeInsets.all(5.0),      
+              ),
+            ]
+          );
+  }    
 }
